6799da55a33a8b9559d2675ea6dc091899e0d9a8
warvan123 <carlylevere@gmail.com>
2026-03-18 04:43:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,12 @@ function App() {
   return (
     <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
       <div className="mt-12 text-5xl font-bold flex items-center z-10">
-        <img src={coin} width={44} height={44} />
+        <img src={coin} width={44} height={44} alt="Coin" />
         <span className="ml-2">{points.toLocaleString()}</span>
       </div>
       <div className="text-[#ffd334] font-medium flex items-center z-10">
-        <img src={trophy} width={24} height={24} />
-        <span className="ml-1">Gold <img src={rocket} width={24} height={24} /></span>
+        <img src={trophy} width={24} height={24} alt="Trophy" />
+        <span className="ml-1">Gold <img src={rocket} width={24} height={24} alt="Rocket" /></span>
       </div>
 
       <div className="flex-grow flex items-center justify-center">
@@ -48,7 +48,7 @@ function App() {
       <div className="fixed bottom-0 left-0 w-full px-4 pb-4 z-10">
         <div className="w-full flex justify-between items-center mb-4">
           <div className="flex items-center">
-            <img src={highVoltage} width={44} height={44} />
+            <img src={highVoltage} width={44} height={44} alt="Energy" />
             <div className="ml-2 text-left">
               <span className="text-white text-2xl font-bold block">{energy}</span>
               <span className="text-white text-secondary opacity-75">/ 6500</span>
