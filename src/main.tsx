e95d54d04f465c4a6c4a323fdde2d06e5747a9c3
warvan123 <carlylevere@gmail.com>
2026-03-18 18:21:01 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+// ئەڤە فایلی سەرەکی یێ پڕۆژەی یە
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* ئەڤە وادەکەت وەلێت کار بکەت */}
-    <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json">
+    {/* لێرە لینکی مانێفێستێکی فەرمی دادەنێین بۆ ئەوەی وەلێت بێ کێشە ببەسترێتەوە.
+      ئەمە کێشەی "Invalid Manifest" چارەسەر دەکات کە لە وێنەکەدا دیار بوو.
+    */}
+    <TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>,
