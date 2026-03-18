6492441575a78b79c114737645fb8e65c57b50d4
warvan123 <carlylevere@gmail.com>
2026-03-18 05:07:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
-import './App.css';
-import { coin, highVoltage, notcoin, rocket, trophy } from './assets/images';
-
+import './App.css'
+import { coin, highVoltage, onecoin, rocket, trophy } from './assets/images
 function App() {
   const [points, setPoints] = useState(0);
   const [energy, setEnergy] = useState(6500);
